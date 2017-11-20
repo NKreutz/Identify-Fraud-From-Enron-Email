8bb4cf8534bc773bf904f76b54d7b3e9fbc50246
Narissa Kreutz <narissa.kreutz@gmail.com>
2017-11-20 16:48:41 -0700

    first commit

--- final_project/poi_id.py
+++ final_project/poi_id.py
@@ -1,25 +1,71 @@
 #!/usr/bin/python
 
 import sys
-import pickle
+import cPickle as pickle
+from time import time
 sys.path.append("../tools/")
 
 from feature_format import featureFormat, targetFeatureSplit
 from tester import dump_classifier_and_data
 
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.naive_bayes import GaussianNB
+from sklearn.grid_search import GridSearchCV
+from sklearn.feature_selection import SelectKBest
+from sklearn.cross_validation import StratifiedShuffleSplit
+
 ### Task 1: Select what features you'll use.
 ### features_list is a list of strings, each of which is a feature name.
 ### The first feature must be "poi".
-features_list = ['poi','salary'] # You will need to use more features
+features_list = ['poi', 'salary', 'deferral_payments', 'loan_advances',
+                'bonus', 'deferred_income', 'expenses','long_term_incentive',
+                'other', 'total_payments', 'director_fees',
+                'exercised_stock_options', 'total_stock_value',
+                'restricted_stock', 'restricted_stock_deferred',
+                'to_messages', 'combined_pay_and_stock',
+                'from_poi_to_this_person','from_messages',
+                'from_this_person_to_poi','shared_receipt_with_poi',
+                'proportion_pay_to_salary']
+# kept most of the features because I use univariate feature selection later,
+# removed 'email_address' as throwing an error (not able to convert to float)
 
 ### Load the dictionary containing the dataset
 with open("final_project_dataset.pkl", "r") as data_file:
     data_dict = pickle.load(data_file)
 
 ### Task 2: Remove outliers
+data_dict.pop("TOTAL") # removes the "total" outlier that sums up financials
+
+
 ### Task 3: Create new feature(s)
-### Store to my_dataset for easy export below.
 my_dataset = data_dict
+# New features:
+# 1: Combine totals for payments and stock values.
+# 2: Proportion of payments made above base salary to base salary
+for key, value in my_dataset.iteritems():
+    total_payments = value['total_payments']
+    total_stock_value = value['total_stock_value']
+    salary = value['salary']
+
+    #create combined_payments_and_stock_value feature
+    if total_payments != 'NaN' and total_stock_value != 'NaN':
+        value['combined_pay_and_stock'] = total_payments + total_stock_value
+    # If there is no total_payment value, set combined to stock value,
+    # which will then be NaN if stock is also NaN
+    elif total_payments == 'NaN':
+        value['combined_pay_and_stock'] = total_stock_value
+    # If there is a total_payment value but stock is NaN,
+    # set combined value to payment value
+    else:
+        value['combined_pay_and_stock'] = total_payments
+
+    # create proportion_payments_to_salary feature:
+    if total_payments != 'NaN' and salary != 'NaN':
+        value['proportion_pay_to_salary'] = (total_payments - salary) / salary
+    # If either value is NaN, set proportion_payments_to_salary to NaN
+    else:
+        value['proportion_pay_to_salary'] = 'NaN'
 
 ### Extract features and labels from dataset for local testing
 data = featureFormat(my_dataset, features_list, sort_keys = True)
@@ -27,25 +73,25 @@ labels, features = targetFeatureSplit(data)
 
 ### Task 4: Try a varity of classifiers
 ### Please name your classifier clf for easy export below.
-### Note that if you want to do PCA or other multi-stage operations,
-### you'll need to use Pipelines. For more info:
-### http://scikit-learn.org/stable/modules/pipeline.html
 
-# Provided to give you a starting point. Try a variety of classifiers.
-from sklearn.naive_bayes import GaussianNB
-clf = GaussianNB()
 
-### Task 5: Tune your classifier to achieve better than .3 precision and recall 
-### using our testing script. Check the tester.py script in the final project
-### folder for details on the evaluation method, especially the test_classifier
-### function. Because of the small size of the dataset, the script uses
-### stratified shuffle split cross validation. For more info: 
-### http://scikit-learn.org/stable/modules/generated/sklearn.cross_validation.StratifiedShuffleSplit.html
+estimators = [('scaler', MinMaxScaler()), ('skb', SelectKBest()),
+                ('gnb', GaussianNB())]
+pipe = Pipeline(estimators)
 
-# Example starting point. Try investigating other evaluation techniques!
-from sklearn.cross_validation import train_test_split
-features_train, features_test, labels_train, labels_test = \
-    train_test_split(features, labels, test_size=0.3, random_state=42)
+param_grid = dict(skb__k=range(2,7))
+
+
+### Task 5: Tune your classifier to achieve better than .3 precision and recall
+### using tester.py
+
+cv = StratifiedShuffleSplit(labels, test_size=0.3, random_state = 42)
+
+gs = GridSearchCV(pipe, param_grid=param_grid, cv=cv, scoring='f1_weighted')
+
+gs.fit(features, labels)
+
+clf = gs.best_estimator_
 
 ### Task 6: Dump your classifier, dataset, and features_list so anyone can
 ### check your results. You do not need to change anything below, but make sure
